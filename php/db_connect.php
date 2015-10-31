89f599ca732c97bf413f6c2ed8d36bd06a3a862b
Robert Colca <robery@cncdloga.ro>
2015-10-31 22:26:40 +0200

    renamed database variable to fix mysqli link issue

--- php/db_connect.php
+++ php/db_connect.php
@@ -2,7 +2,7 @@
 	$host = "";
 	$username = "";
 	$password = "";
-	$db = "Romana-e-okay";
+	$database = "Romana-e-okay";
 	$DB = new MySQLI ($host, $username, $password, $database)
 	//@mysql_select_db($db);
 	error_reporting(0);
