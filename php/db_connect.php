e4fb5976418ba7a2769ccfbec64c7e0926a54195
Robert Colca <robery@cncdloga.ro>
2015-10-31 22:27:38 +0200

    Added development_mode debugging

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,9 +1,10 @@
 <?php
+	define('development_mode', false);
 	$host = "";
 	$username = "";
 	$password = "";
 	$database = "Romana-e-okay";
 	$DB = new MySQLI ($host, $username, $password, $database)
 	//@mysql_select_db($db);
-	error_reporting(0);
+	error_reporting(development_mode ? E_ALL : 0);
 ?>
